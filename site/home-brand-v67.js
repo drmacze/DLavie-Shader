7ac6167b954b4a654ac9a53374fd7611e34eef8e
Drmacze DEV <drma801@gmail.com>
2026-08-30 20:29:13 +0700

    Use v68 GitHub and Minecraft brand assets on Home

--- site/home-brand-v67.js
+++ site/home-brand-v67.js
@@ -3,7 +3,7 @@
   const card=document.querySelector('.project-card');
   if(card&&!card.querySelector('.home-mc-brand')){
     const tags=card.querySelector('.tag-row');
-    const badge='<span class="home-mc-brand"><img src="assets/icon-minecraft-block.svg?v=67" alt=""><span>Minecraft Bedrock</span></span>';
+    const badge='<span class="home-mc-brand"><img src="assets/icon-minecraft-block.svg?v=68" alt=""><span>Minecraft Bedrock</span></span>';
     if(tags)tags.insertAdjacentHTML('beforebegin',badge);else card.insertAdjacentHTML('beforeend',badge);
   }
   const footer=document.querySelector('.footer .wrap');
@@ -11,6 +11,6 @@
     footer.insertAdjacentHTML('beforeend','<span class="home-brand-note">DLavie adalah project independen dan tidak berafiliasi dengan Mojang atau Microsoft.</span>');
   }
   document.querySelectorAll('a[href*="github.com/drmacze/DLavie-Shader"]').forEach(a=>{
-    if(!a.querySelector('img')){a.classList.add('home-github-link');a.insertAdjacentHTML('afterbegin','<img src="assets/icon-github.svg?v=67" alt="">')}
+    if(!a.querySelector('img')){a.classList.add('home-github-link');a.insertAdjacentHTML('afterbegin','<img src="assets/icon-github.svg?v=68" alt="GitHub">')}
   });
 })();
